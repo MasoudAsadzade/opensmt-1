f4af3d0932bd50958d9ba48e4a4cb1379a603110
Masoud Asadzadeh <masoud.asadzade@gmail.com>
2021-03-16 23:59:20 +0100

    add aws batch comp-2021

--- awcCloudTrack/make_opensmt.sh
+++ awcCloudTrack/make_opensmt.sh
@@ -3,5 +3,5 @@
 set -ev
 mkdir build 
 cd build
-cmake -DCMAKE_BUILD_TYPE=${CMAKE_BUILD_TYPE} -DCMAKE_INSTALL_PREFIX=${INSTALL} ..
+cmake -DCMAKE_BUILD_TYPE=${CMAKE_BUILD_TYPE} -DCMAKE_INSTALL_PREFIX=/home/opensmt-1 ..
 make -j4
