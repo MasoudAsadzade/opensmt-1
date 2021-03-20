3ecb25f763c568f95520e76792d36aed4111d8e5
Masoud Asadzadeh <masoud.asadzade@gmail.com>
2021-03-21 00:03:30 +0100

    aws batch

--- awcCloudTrack/awsRunBatch/mpi-run.sh
+++ awcCloudTrack/awsRunBatch/mpi-run.sh
@@ -43,8 +43,11 @@ wait_for_nodes () {
 #  echo "$ip slots=$availablecores" >> $HOST_FILE_PATH
   echo "$ip" >> $HOST_FILE_PATH
   lines=$(ls -dq /tmp/hostfile* | wc -l)
+  echo "lines:"
+  echo lines
   while [ "${AWS_BATCH_JOB_NUM_NODES}" -gt "${lines}" ]
   do
+    cat /etc/hostfile
     cat $HOST_FILE_PATH
     lines=$(ls -dq /tmp/hostfile* | wc -l)
 
@@ -55,10 +58,13 @@ wait_for_nodes () {
 
   # All of the hosts report their IP and number of processors. Combine all these
   # into one file with the following script:
+  echo "supervised-scripts/make_combined_hostfile.py ${ip}:"
   python3 supervised-scripts/make_combined_hostfile.py ${ip}
   cat supervised-scripts/combined_hostfile
-
+  echo "reachhere-s"
+  sleep 2
   time mpirun --mca btl_tcp_if_include eth0 --allow-run-as-root -np ${AWS_BATCH_JOB_NUM_NODES} --hostfile supervised-scripts/combined_hostfile run_aws_osmt.sh "regression/QF_UF/iso_brn029_simplified_1b.smt2"
+  echo "reachhere-e"
   #time mpirun --mca btl_tcp_if_include eth0 --allow-run-as-root -np ${AWS_BATCH_JOB_NUM_NODES} --hostfile supervised-scripts/combined_hostfile run_aws_osmt.sh "hpcClusterBenchs"
 }
 
