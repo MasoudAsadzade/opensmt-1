aa8cb6e02e2a9115fcfad8abdefb81abcbafe9fe
Masoud Asadzadeh <masoud.asadzade@gmail.com>
2021-03-21 02:27:00 +0100

    aws batch

--- awcCloudTrack/awsRunBatch/run_aws_osmt.sh
+++ awcCloudTrack/awsRunBatch/run_aws_osmt.sh
@@ -1,12 +1,7 @@
   #!/bin/bash
 
-if [ $# != 1 ]; then
-    echo "Usage: $0 <provide shared bucket dir>"
-    exit 1
-fi
-
 #for folder in "$1"/*; do
   #  for file in "$folder"/*/*; do
-      ./build/src/bin/opensmt $1
+      ./opensmt-1/build/src/bin/opensmt opensmt-1/hpcClusterBenchs/QF_UF/unsat/NEQ_NEQ048_size8.smt2
  #   done
 #done
